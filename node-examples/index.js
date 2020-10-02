19c45bab62cac6f70f0e27f5d7eddc323d798a28
Victoria Bodnar <bodnarvik@gmail.com>
2020-10-02 16:26:58 +0300

    excersice 1,2 illustrate callback function

--- node-examples/index.js
+++ node-examples/index.js
@@ -10,14 +10,20 @@ function solveRect(l,b) {
 
 	console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-	if (l <= 0 || b <=0) {
+	rect(l,b,(err, rectangle) => {
+		if (err) {
+			
+			console.log("ERROR: ", err.message);
+		
+		}else{
 
-		console.log("Rectangle dimension sould be greater than zero: l = " + l + " and b = " + b);
-	}else{
+			console.log("The area of the rectangle of dimentions l = " + l + " and b = " + b + " is: " + rectangle.area());
+			console.log("The perimeter of the rectangle of dimentions l = " + l + " and b = " + b + " is: " + rectangle.perimeter());
+		}
 
-		console.log("The area of the rectangle is " + rect.area(l,b));
-		console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-	}
+	});
+	console.log("This statement is after the call to rect()");
+
 }
 solveRect(2,4);
 solveRect(3,5);
