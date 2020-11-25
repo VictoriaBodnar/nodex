3e60d23f33bf0d433cf3355e6b415cc3d030bbd1
Victoria Bodnar <bodnarvik@gmail.com>
2020-11-25 09:33:34 +0200

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,9 @@
 module.exports = {
 	'secretKey': '12345-67890-09876-54321',
-	'mongoURL': 'mongodb://localhost:27017/conFusion'
+	'mongoURL': 'mongodb://localhost:27017/conFusion',
+	'facebook': {
+		clientId: '390016115550305',
+		clientSecret: '950e1192ad6b6454bfb1bd423dad4689'
+
+	}
 }
